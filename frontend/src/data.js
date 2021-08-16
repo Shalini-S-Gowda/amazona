91d91acdd0a36b8b3d0f6b9ded6e7601453cdb51
Shalini Gowda <shalinigowdthi@gmail.com>
2021-08-16 21:00:56 +0530

    initial project source code

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Full Zip Running Shirts',
+            category: 'Shirts',
+            image: '/images/product-1.jpg',
+            price: $60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '2',
+            name: 'Nike Full Zip Hoodie',
+            category: 'Shirts',
+            image: '/images/product-2.jpg',
+            price: $70,
+            brand: 'Nike',
+            rating: 4.6,
+            numReviews: 20,
+            countInStock: 6
+        },
+        {
+            _id: '3',
+            name: 'Adidas Long Sleeve Hoodie',
+            category: 'Shirts',
+            image: '/images/product-3.jpg',
+            price: $90,
+            brand: 'Adidas',
+            rating: 5,
+            numReviews: 20,
+            countInStock: 6
+        },
+        {
+            _id: '4',
+            name: 'Under Armour Sports Joggers',
+            category: 'Pants',
+            image: '/images/product-4.jpg',
+            price: $70,
+            brand: 'Under Armour',
+            rating: 4.6,
+            numReviews: 20,
+            countInStock: 6
+        },
+        {
+            _id: '5',
+            name: 'Champion Graphic Powerblend Fleece Jogger',
+            category: 'Pants',
+            image: '/images/product-5.jpg',
+            price: $70,
+            brand: 'Champion',
+            rating: 4.6,
+            numReviews: 20,
+            countInStock: 6
+        },
+        {
+            _id: '6',
+            name: 'Aelfric Eden Jogger Pants Lonh Multi-Pockets',
+            category: 'Pants',
+            image: '/images/product-6.jpg',
+            price: $70,
+            brand: 'Aelfric Eden',
+            rating: 4.6,
+            numReviews: 20,
+            countInStock: 6
+        },
+    ],
+};
